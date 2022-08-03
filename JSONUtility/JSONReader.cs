251a3715dd957c396f9b34ce6b0289e3e814998e
Rimuru <Rimuru.Dev@gmail.com>
2022-08-03 22:47:06 +0300

    Create JSONReader.cs

---
+++ JSONUtility/JSONReader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.JSONUtility
+{
+    [System.Serializable]
+    public sealed class JSONReader<T>
+    {
+        private string path;
+
+        public T Read(string pathToJSONData)
+        {
+            using WWW data = new WWW($"{Application.streamingAssetsPath}/{pathToJSONData}");
+
+            var tmpPlayer = JsonUtility.FromJson<T>(data.text);
+
+            return tmpPlayer;
+        }
+
+        public T Read()
+        {
+            using WWW data = new WWW($"{Application.streamingAssetsPath}/{path}");
+
+            var tmpPlayer = JsonUtility.FromJson<T>(data.text);
+
+            return tmpPlayer;
+        }
+
+        public JSONReader() { }
+
+        public JSONReader(string path) => this.path = path;
+    }
+}
