5ef09ff0e2f2d55d97daebdc8badff34dfcab696
Rimuru <Rimuru.Dev@gmail.com>
2022-08-03 22:47:07 +0300

    Create JSONWriter.cs

---
+++ JSONUtility/JSONWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace RimuruDev.JSONUtility
+{
+    [Serializable]
+    public sealed class JSONWriter
+    {
+        private string path;
+        private object obj;
+
+        public void Write(string path, object obj) =>
+            File.WriteAllText($"{Application.streamingAssetsPath}/{path}", $"{JsonUtility.ToJson(obj: obj)}");
+
+        public void Write() =>
+           File.WriteAllText($"{Application.streamingAssetsPath}/{path}", $"{JsonUtility.ToJson(obj: obj)}");
+
+        public JSONWriter() { }
+
+        public JSONWriter(string pathToJson, object obj)
+        {
+            this.path = pathToJson;
+            this.obj = obj;
+        }
+    }
+}
